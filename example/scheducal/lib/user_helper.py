3815f18bc342a7184b5051e1bac404ac332dd83d
Jordan Enterkin <jordan.a.enterkin@gmail.com>
2013-08-27 21:41:26 -0400

    derp

---
+++ example/scheducal/lib/user_helper.py
@@ -0,0 +1,9 @@
+def user_dict(user):
+    groups = [group.pk for group in user.groups.all()]
+    return {
+        'id': user.pk,
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'groups': groups or None,
+    }
