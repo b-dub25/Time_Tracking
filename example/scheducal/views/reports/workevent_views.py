04ab7c08bdc8da9b69a5dcea91c364693dc949e1
Jordan Enterkin <jordan.a.enterkin@gmail.com>
2013-09-20 10:30:22 -0400

    subtracting minute

--- example/scheducal/views/reports/workevent_views.py
+++ example/scheducal/views/reports/workevent_views.py
@@ -28,7 +28,8 @@ def work_event_list_for_pay_period(request, pay_period):
     events = []
     for user in User.objects.all():
         try:
-            new_end = pay_period.end + datetime.timedelta(days=1)
+            new_end = pay_period.end + datetime.timedelta(days=1) \
+                      - datetime.timedelta(minutes=1)
             user_events = WorkEvent.objects \
                           .filter(user=user,
                                   start__range=[pay_period.start, \
